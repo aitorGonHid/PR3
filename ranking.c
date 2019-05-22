dc4506ca127e823c32d13482e4965580dcafe575
Aitor Gonzalez <aitorgh@gmail.com>
2019-05-22 17:54:28 +0200

    rankingList_insert OK

--- ranking.c
+++ ranking.c
@@ -127,8 +127,78 @@ tRankingListNode* rankingList_getNode(tRankingList* list, int index){
 
 // Insert/adds a new ranking to the ranking list
 tError rankingList_insert(tRankingList* list, tRanking ranking, int index){
-    // PR3 EX2
-    return ERR_NOT_IMPLEMENTED;
+	// PR3 EX2
+    //preconditions check
+	assert (list != NULL);
+	
+	//variable declaration
+	tRankingListNode *tmp;
+	tRankingListNode *aux;
+	int i, size;
+	
+	//verify index
+	size = list->size;
+	if (index < 1 || index > size+1) {
+		return ERR_INVALID_INDEX;
+	}
+
+	tmp = (tRankingListNode*) malloc (sizeof(tRankingListNode));
+	
+	//verify memory allocation	
+	if (tmp == NULL){
+		return ERR_MEMORY_ERROR;
+	}
+	
+	//insert element in index position
+	if (list->size == 0) {
+		//empty list
+		if (index == 1) {
+			tmp->next = NULL;
+			tmp->prev = NULL;
+			tmp->e = ranking;
+			list->first = tmp;
+			list->last = tmp;
+			list->size = list->size + 1;
+		} else {
+			return ERR_INVALID_INDEX;
+		}
+	} else {
+		//list with some elements
+		if (index == 1){
+			//insert at first position
+			tmp->next = list->first;
+			tmp->prev = NULL;
+			tmp->e = ranking;
+			list->first->prev = tmp;
+			list->first = tmp;
+			list->size = list->size + 1;
+		} else if (index == list->size+1){
+			//insert at last position
+			tmp->next = NULL;
+			tmp->prev = list->last;
+			tmp->e = ranking;
+			list->last->next = tmp;
+			list->last = tmp;
+			list->size = list->size+1;
+		} else {
+			//insert between 1st and last pos
+			i = 1;
+			aux = list->first;
+			//get position where new element must be in
+			while (i < index) {
+				aux = aux->next;
+				i++;
+			}
+			//insert element and actualize the previous and next element pointers
+			tmp->next = aux;
+			tmp->prev = aux->prev;
+			tmp->e = ranking;
+			aux->prev->next=tmp;
+			aux->prev=tmp;
+			list->size = list->size + 1;
+		}
+	}
+	return OK;
 }
 
 // Deletes a ranking from the ranking list
@@ -178,4 +248,3 @@ tRankingList* rankingList_sortInsertion(tRankingList *list){
     // PR3 EX3
     return NULL;
 }
-
